d59e7cb35db635fc55823249a53dab84735a2821
Resurgence72 <wuhaoyang1105@qq.com>
2023-06-06 14:23:57 +0800

    Update operator.go

--- pkg/provider/ali/operator.go
+++ pkg/provider/ali/operator.go
@@ -404,7 +404,7 @@ func (o *operator) commonRequest(
 	}
 
 	resp, err := retry(client, request, 5)
-	if err != nil || (resp != nil && !resp.IsSuccess()) {
+	if err != nil || (resp != nil && !resp.IsSuccess()) || (resp.BaseResponse != nil && !resp.BaseResponse.IsSuccess()) {
 		return nil, err
 	}
 	return resp.BaseResponse.GetHttpContentBytes(), nil
