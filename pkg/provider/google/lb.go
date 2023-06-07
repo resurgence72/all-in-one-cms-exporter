e3ab54bd0f69ecae236ab10dcc034f3c8ece5aa7
Resurgence72 <wuhaoyang1105@qq.com>
2023-06-07 15:24:09 +0800

    Update lb.go

--- pkg/provider/google/lb.go
+++ pkg/provider/google/lb.go
@@ -56,6 +56,10 @@ func (l *Lb) push(transfer *transferData) {
 			return
 		}
 
+		point := points[len(points)-1]
+		ts := point.Interval.EndTime.GetSeconds()
+		value := l.op.getPointValue(series.GetValueType(), point)
+		
 		metricLabels := series.Metric.Labels
 		resourceLabels := series.Resource.Labels
 		ep, ok := resourceLabels["url_map_name"]
@@ -64,88 +68,83 @@ func (l *Lb) push(transfer *transferData) {
 		}
 
 		quantileIdx := []string{"p50", "p90", "p99", "mean"}
-		for _, point := range points {
-			ts := point.Interval.EndTime.GetSeconds()
-			value := l.op.getPointValue(series.GetValueType(), point)
+		switch v := value.(type) {
+		case quantileContainer:
+			for i, quantile := range v.qs {
+				series := &common.MetricValue{
+					Metric:       common.BuildMetric("lb", metricName),
+					Endpoint:     ep,
+					Timestamp:    ts,
+					ValueUntyped: quantile,
+					TagsMap: map[string]string{
+						"metric_kind":  transfer.metric.MetricKind.String(),
+						"value_type":   transfer.metric.ValueType.String(),
+						"unit":         transfer.metric.Unit,
+						"launch_stage": transfer.metric.LaunchStage.String(),
 
-			switch v := value.(type) {
-			case quantileContainer:
-				for i, quantile := range v.qs {
-					series := &common.MetricValue{
-						Metric:       common.BuildMetric("lb", metricName),
-						Endpoint:     ep,
-						Timestamp:    ts,
-						ValueUntyped: quantile,
-						TagsMap: map[string]string{
-							"metric_kind":  transfer.metric.MetricKind.String(),
-							"value_type":   transfer.metric.ValueType.String(),
-							"unit":         transfer.metric.Unit,
-							"launch_stage": transfer.metric.LaunchStage.String(),
-
-							"provider":  ProviderName,
-							"iden":      l.op.req.Iden,
-							"namespace": l.namespace,
-						},
-					}
-
-					if pid, ok := resourceLabels["project_id"]; ok {
-						series.TagsMap["project_id"] = pid
-
-						if pname, ok := l.op.projects.Load(pid); ok {
-							series.TagsMap["project_mark"] = pname.(string)
-						}
-					}
-					if ccode, ok := metricLabels["response_code"]; ok {
-						series.TagsMap["response_code"] = ccode
-					}
-					if cc, ok := metricLabels["response_code_class"]; ok {
-						series.TagsMap["response_code_class"] = cc
-					}
-
-					series.TagsMap["quantile"] = quantileIdx[i]
-					series.BuildAndShift()
+						"provider":  ProviderName,
+						"iden":      l.op.req.Iden,
+						"namespace": l.namespace,
+					},
 				}
-				continue
 
-			default:
-			}
+				if pid, ok := resourceLabels["project_id"]; ok {
+					series.TagsMap["project_id"] = pid
 
-			series := &common.MetricValue{
-				Metric:       common.BuildMetric("lb", metricName),
-				Endpoint:     ep,
-				Timestamp:    ts,
-				ValueUntyped: value,
-			}
-
-			series.TagsMap = map[string]string{
-				"metric_kind":  transfer.metric.MetricKind.String(),
-				"value_type":   transfer.metric.ValueType.String(),
-				"unit":         transfer.metric.Unit,
-				"launch_stage": transfer.metric.LaunchStage.String(),
-
-				"provider":  ProviderName,
-				"iden":      l.op.req.Iden,
-				"namespace": l.namespace,
-			}
-
-			if pid, ok := resourceLabels["project_id"]; ok {
-				series.TagsMap["project_id"] = pid
-
-				if pname, ok := l.op.projects.Load(pid); ok {
-					series.TagsMap["project_mark"] = pname.(string)
+					if pname, ok := l.op.projects.Load(pid); ok {
+						series.TagsMap["project_mark"] = pname.(string)
+					}
 				}
-			}
+				if ccode, ok := metricLabels["response_code"]; ok {
+					series.TagsMap["response_code"] = ccode
+				}
+				if cc, ok := metricLabels["response_code_class"]; ok {
+					series.TagsMap["response_code_class"] = cc
+				}
 
-			if ccode, ok := metricLabels["response_code"]; ok {
-				series.TagsMap["response_code"] = ccode
+				series.TagsMap["quantile"] = quantileIdx[i]
+				series.BuildAndShift()
 			}
+			continue
 
-			if cc, ok := metricLabels["response_code_class"]; ok {
-				series.TagsMap["response_code_class"] = cc
-			}
-
-			series.BuildAndShift()
+		default:
 		}
+
+		series := &common.MetricValue{
+			Metric:       common.BuildMetric("lb", metricName),
+			Endpoint:     ep,
+			Timestamp:    ts,
+			ValueUntyped: value,
+		}
+
+		series.TagsMap = map[string]string{
+			"metric_kind":  transfer.metric.MetricKind.String(),
+			"value_type":   transfer.metric.ValueType.String(),
+			"unit":         transfer.metric.Unit,
+			"launch_stage": transfer.metric.LaunchStage.String(),
+
+			"provider":  ProviderName,
+			"iden":      l.op.req.Iden,
+			"namespace": l.namespace,
+		}
+
+		if pid, ok := resourceLabels["project_id"]; ok {
+			series.TagsMap["project_id"] = pid
+
+			if pname, ok := l.op.projects.Load(pid); ok {
+				series.TagsMap["project_mark"] = pname.(string)
+			}
+		}
+
+		if ccode, ok := metricLabels["response_code"]; ok {
+			series.TagsMap["response_code"] = ccode
+		}
+
+		if cc, ok := metricLabels["response_code_class"]; ok {
+			series.TagsMap["response_code_class"] = cc
+		}
+
+		series.BuildAndShift()
 	}
 }
 
